169a0269f2e4eef97a6dd8bce68cb5855839e598
Jake Green <jaketg10@gmail.com>
2017-11-08 17:10:18 +0000

    Update sv_sql.lua

---
+++ gamemode/core/sv_sql.lua
@@ -0,0 +1,10 @@
+-- Uses https://facepunch.com/showthread.php?t=1515853
+require("mysqloo")
+
+function impulse.DB.boot()
+    impulse.DB.object = mysqloo.connect( impulse.DB.ip, impulse.DB.username, impulse.DB.password, [ impulse.DB.database, port, socket] )
+    local database = impulse.DB.object
+    database.onConnected = function() MsgC(Color(0,255,0),"[IMPULSE] Database connection established.\n") end
+	database.onConnectionFailed = function() MsgC(Color(0,255,0),"[IMPULSE] Database connection failure.\n") end
+	database:connect()
+end
