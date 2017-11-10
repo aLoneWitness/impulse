2aceea0a810b1025b8afe3418aa326c9dfba4f4e
Jake Green <jaketg10@gmail.com>
2017-11-10 15:10:35 +0000

    Added query function with advanced error catching, started work on player auth, remade boot and created basetables code.

--- gamemode/core/sv_sql.lua
+++ gamemode/core/sv_sql.lua
@@ -1,20 +1,55 @@
 -- Uses https://facepunch.com/showthread.php?t=1515853
 require("mysqloo")
 
+local baseTables = [[
+CREATE TABLE IF NOT EXISTS `impulse_pd` (
+	`_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`_name` varchar(70) NOT NULL,
+	`_group` text NOT NULL,
+	`_cosmetic` varchar(180) NOT NULL,
+	`_attributes` varchar(180) DEFAULT NULL,
+	`_schema` varchar(24) NOT NULL,
+	`_rpgroup` int(11) unsigned NOT NULL,
+	`_steamid` bigint(20) unsigned NOT NULL,
+	`_data` longtext,
+	`_money` int(11) unsigned DEFAULT NULL,
+	`_bankmoney` int(11) unsigned DEFAULT NULL,
+	`_rank` varchar(50) NOT NULL,
+	'_blacklists' varchar(180) NOT NULL,
+	PRIMARY KEY (`_id`)
+);
+
+
+
+]]
+
 function impulse.DB.boot()
     impulse.DB.object = mysqloo.connect( impulse.DB.ip, impulse.DB.username, impulse.DB.password, impulse.DB.database, 3306 )
     local database = impulse.DB.object
-    database.onConnected = function() 
-        MsgC(Color(0,255,0),"[IMPULSE] Database connection established.\n") 
+    function database:onConnected()
+        MsgC(Color(0,255,0),"[IMPULSE] Database connection established.\n")
+        self:query(baseTables)
     end
-	database.onConnectionFailed = function(error) 
-	    MsgC(Color(0,255,0),"[IMPULSE] Database connection failure. Error dump: ("..error..")\n") 
+	function database:onConnectionFailed(error) 
+	    return MsgC(Color(0,255,0),"[IMPULSE] Database connection failure. Error dump: ("..error..")\n") 
 	end
 	database:connect()
+	
 end
 
-local function dbAuth()
+function impulse.DB.query(data,name)
+    local query = impulse.DB.object:query(data)
     
+    function query:onError(err,sql)
+       MsgC(Color(255,0,0),"[IMPULSE] Query failure! (NAME: "..name.." ERROR: "..err.." QUERY: "..sql) 
+       -- Logging system coming soon.
+    end
+    
+    query:start()
+end
+
+local function dbAuth(player,id)
+    if 
     
 end
 hook.Add("PlayerAuthed","IMPULSE-DATABASE-AUTH",dbAuth)
