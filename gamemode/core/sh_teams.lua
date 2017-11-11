fc4df8c1dffee4e8d2f699257d45c086d9cbb229
Jake Green <jaketg10@gmail.com>
2017-11-11 12:50:23 +0000

    Added simple team definer.

--- gamemode/core/sh_teams.lua
+++ gamemode/core/sh_teams.lua
@@ -1,8 +1,6 @@
 impulse.team = {}
 
 function impulse.team.define(data_table)
-
-
-
-
+    team.SetUp( data_table.id, data_table.name, data_table.color ) -- Hooking into the source engine team system
+    return data_table.id -- return the teams unique id to be stored as the team name EG: TEAM_VISITOR
 end
