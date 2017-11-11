1440e41092c94c3198fd405672b247238535efc7
Jake Green <jaketg10@gmail.com>
2017-11-11 14:59:26 +0000

    Properly added iscp function, created team table system.

--- gamemode/core/sh_teams.lua
+++ gamemode/core/sh_teams.lua
@@ -3,12 +3,10 @@ impulse.team.teams = {}
 
 function impulse.team.define(data_table)
     team.SetUp( data_table.id, data_table.name, data_table.color ) -- Hooking into the source engine team system
-    TEAM = impulse.team.teams[data_table.name]
-    TEAM = nil
+    impulse.team.teams[data_table.id] = data_table
     return data_table.id -- return the teams unique id to be stored as the team name EG: TEAM_VISITOR
 end
 
 function meta:IsCP() -- function to check if the team has authority
-    if self:Team()
-    
+    return impulse.team.teams[self:Team()].iscp
 end
