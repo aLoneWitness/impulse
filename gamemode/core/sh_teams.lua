c210884d0c834d50f3bcf206b1551e0e05ee826e
Jake Green <jaketg10@gmail.com>
2017-11-11 13:02:14 +0000

    Update sh_teams.lua

--- gamemode/core/sh_teams.lua
+++ gamemode/core/sh_teams.lua
@@ -1,7 +1,9 @@
 impulse.team = {}
+impulse.team.teams = {}
 
 function impulse.team.define(data_table)
     team.SetUp( data_table.id, data_table.name, data_table.color ) -- Hooking into the source engine team system
+    TEAM = imp
     return data_table.id -- return the teams unique id to be stored as the team name EG: TEAM_VISITOR
 end
 
