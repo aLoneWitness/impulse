1654b9c4719b2607f08ee26559fbb3ad7445aed7
Jake Green <jaketg10@gmail.com>
2017-11-11 12:56:04 +0000

    Update sh_teams.lua

--- gamemode/core/sh_teams.lua
+++ gamemode/core/sh_teams.lua
@@ -4,3 +4,8 @@ function impulse.team.define(data_table)
     team.SetUp( data_table.id, data_table.name, data_table.color ) -- Hooking into the source engine team system
     return data_table.id -- return the teams unique id to be stored as the team name EG: TEAM_VISITOR
 end
+
+function meta:IsCP() -- function to check if the team has authority
+    if self:Team()
+    
+end
