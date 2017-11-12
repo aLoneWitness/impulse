be38f6fdee92b9e4cf96c4fe99c5e952eab790ae
Jake Green <jaketg10@gmail.com>
2017-11-12 19:03:55 +0000

    Added schema file and plugin support.

--- gamemode/core/sh_schema.lua
+++ gamemode/core/sh_schema.lua
@@ -2,6 +2,15 @@ impulse.schema = impulse.schema or {}
 
 function impulse.schema.boot()
     SCHEMA = {GM.FolderName}
-    -- PLUGIN LOADING AND TEAM/CONFIG LOADING HERE
+    MsgC( Color( 83, 143, 239 ), "[IMPULSE] Loading '"..SCHEMA.."' schema...\n" )
+    impulse.lib.includeDir(SCHEMA.."/config")
+    impulse.lib.includeDir(SCHEMA.."/autorun")
+
+    for files, dir in ipairs(file.Find(SCHEMA.."/plugins/*", "LUA")) do
+        MsgC( Color( 83, 143, 239 ), "[IMPULSE] ["..SCHEMA.."] Loading plugin '"..dir.."'\n" )
+	    impulse.lib.includeDir(dir)
+    end
+
+end
     hook.Run('SchemaLoad')
 end
