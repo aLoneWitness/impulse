be25d527f51d298468b3cf200aff63bc3201423d
Jake Green <jaketg10@gmail.com>
2017-11-12 13:29:42 +0000

    Added model pre-loading.

--- gamemode/core/sh_teams.lua
+++ gamemode/core/sh_teams.lua
@@ -4,6 +4,11 @@ impulse.team.teams = {}
 function impulse.team.define(data_table)
     team.SetUp( data_table.id, data_table.name, data_table.color ) -- Hooking into the source engine team system
     impulse.team.teams[data_table.id] = data_table
+    if (CLIENT) then
+        for v,k in pairs(data_table.models) do
+            impulse.PrepModelForLoad(k)
+        end
+    end
     return data_table.id -- return the teams unique id to be stored as the team name EG: TEAM_VISITOR
 end
 
