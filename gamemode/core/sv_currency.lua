e720d6a8fd913a2a64916b1f1eeb360a58c8308d
Jake Green <jaketg10@gmail.com>
2017-11-12 12:12:54 +0000

    Added setMoney function.

--- gamemode/core/sv_currency.lua
+++ gamemode/core/sv_currency.lua
@@ -1,4 +1,6 @@
 function meta:setMoney(value)
-
-
+    self:setIVar("money",value)
+    return impulse.DB.query("UPDATE impulse_pd SET money = "..value.." WHERE steamid = '"..self:SteamID().." '")
 end
+
+function
