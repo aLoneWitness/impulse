11f6b8baca94b9f1fed427ad8c6be515328bcceb
Jake Green <jaketg10@gmail.com>
2017-11-12 13:26:25 +0000

    Added currency functions: SetMoney CanAfford GiveMoney TakeMoney SetBank -- CanAffordBank, GiveBank, TakeBank coming soon.

--- gamemode/core/sv_currency.lua
+++ gamemode/core/sv_currency.lua
@@ -1,6 +1,20 @@
-function meta:setMoney(value)
+function meta:SetMoney(value)
     self:setIVar("money",value)
     return impulse.DB.query("UPDATE impulse_pd SET money = "..value.." WHERE steamid = '"..self:SteamID().." '")
 end
 
-function
+function meta:CanAfford(cost)
+   return cost <= (self:getIVar("money") or 0) 
+end
+
+function meta:GiveMoney(amount)
+    self:setMoney(self:getIVar("money")+amount)
+end
+
+function meta:TakeMoney(amount)
+    self:setMoney(self:getIVar("money")-amount)
+end
+
+function meta:SetBank(value)
+    return impulse.DB.query("UPDATE impulse_pd SET atm = "..value.." WHERE steamid = '"..self:SteamID().." '")
+end
