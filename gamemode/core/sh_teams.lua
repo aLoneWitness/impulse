29e182fcd3c49bab0f64c319f6d18b2daf5cbd3e
Jake Green <jaketg10@gmail.com>
2017-11-12 16:54:33 +0000

    Update sh_teams.lua

--- gamemode/core/sh_teams.lua
+++ gamemode/core/sh_teams.lua
@@ -15,3 +15,7 @@ end
 function meta:IsCP() -- function to check if the team has authority
     return impulse.team.teams[self:Team()].iscp
 end
+
+function IMPULSE:OnPlayerChangeTeam(player,oldteam,newteam)
+   player:SetIVar("job",team.GetName(newteam))
+end
