54c7604511f5122c025a69401ad85b18ea083a06
Jake Green <jaketg10@gmail.com>
2017-11-12 13:42:26 +0000

    Update sh_teams.lua

--- gamemode/core/sh_teams.lua
+++ gamemode/core/sh_teams.lua
@@ -5,8 +5,8 @@ function impulse.team.define(data_table)
     team.SetUp( data_table.id, data_table.name, data_table.color ) -- Hooking into the source engine team system
     impulse.team.teams[data_table.id] = data_table
     if (CLIENT) then
-        for v,k in pairs(data_table.models) do
-            impulse.PrepModelForLoad(k)
+        for v,k in pairs(data_table.models) do 
+            impulse.PrepModelForLoad(k) -- We will load all the player models when people load
         end
     end
     return data_table.id -- return the teams unique id to be stored as the team name EG: TEAM_VISITOR
