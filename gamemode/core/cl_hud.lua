d1949759a0e6d3f17f2c418ca059f64db052b52d
Jake Green <jaketg10@gmail.com>
2017-11-13 17:38:27 +0000

    Update cl_hud.lua

--- gamemode/core/cl_hud.lua
+++ gamemode/core/cl_hud.lua
@@ -1,5 +1,16 @@
-function IMPULSE:HudShouldDraw(name)
-	if(name == "CHudHealth") or (name == "CHudBattery") then
-	    return false
-    end
+
+local hidden = {}
+hidden["CHudHealth"] = true
+hidden["CHudBattery"] = true
+hidden["CHudAmmo"] = true
+hidden["CHudSecondaryAmmo"] = true
+hidden["CHudCrosshair"] = true
+hidden["CHudHistoryResource"] = true
+
+function IMPULSE:HUDShouldDraw(element)
+	if (hidden[element]) then
+		return false
+	end
+
+	return true
 end
