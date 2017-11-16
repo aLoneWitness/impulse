e757d28a1ccc4e839c5783a5eb3cfc6e000d6482
Jake Green <jaketg10@gmail.com>
2017-11-16 18:14:43 +0000

    Major security fix.

--- gamemode/core/sv_currency.lua
+++ gamemode/core/sv_currency.lua
@@ -3,16 +3,20 @@ function meta:SetMoney(value)
     return impulse.DB.query("UPDATE impulse_pd SET money = "..value.." WHERE steamid = '"..self:SteamID().." '")
 end
 
+function meta:GetMoney()
+   return tonumber(impulse.DB.query("SELECT money FROM impulse_pd WHERE steamid = '"..self:SteamID().."'"))
+end
+
 function meta:CanAfford(cost)
-   return cost <= (self:getIVar("money") or 0) 
+   return cost <= (self:GetMoney() or 0) 
 end
 
 function meta:GiveMoney(amount)
-    return self:setMoney(self:getIVar("money")+amount)
+    return self:setMoney(self:GetMoney()+amount)
 end
 
 function meta:TakeMoney(amount)
-    return self:setMoney(self:getIVar("money")-amount)
+    return self:setMoney(self:GetMoney()-amount)
 end
 
 function meta:SetBank(value)
