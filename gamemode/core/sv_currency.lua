3f541a7551e89888b05a343f470a22ec70261a55
Jake Green <jaketg10@gmail.com>
2017-11-16 18:15:31 +0000

    Update sv_currency.lua

--- gamemode/core/sv_currency.lua
+++ gamemode/core/sv_currency.lua
@@ -1,6 +1,6 @@
 function meta:SetMoney(value)
     self:setIVar("money",value)
-    return impulse.DB.query("UPDATE impulse_pd SET money = "..value.." WHERE steamid = '"..self:SteamID().." '")
+    impulse.DB.query("UPDATE impulse_pd SET money = "..value.." WHERE steamid = '"..self:SteamID().." '")
 end
 
 function meta:GetMoney()
