1893127d4b131e643497700798712fc99c0e14cf
Jake Green <jaketg10@gmail.com>
2017-11-16 18:08:21 +0000

    Change name inventoryGet to inventoryRead, made the function functional.

--- gamemode/core/sv_inventory.lua
+++ gamemode/core/sv_inventory.lua
@@ -2,6 +2,6 @@ function meta:inventorySave()
     return impulse.DB.query("UPDATE impulse_pd SET inventory = "..SQLStr(pon.endcode(self.Inventory)).."WHERE steamid = '"..self:SteamID().."'","INVENTORY-SET")
 end
 
-function meta:inventoryGet()
-    return impulse.DB.query("SELECT inventory FROM impulse_pd WHERE steamid = '"..self:SteamID().."'")
+function meta:inventoryRead()
+    self.Inventory = impulse.DB.query("SELECT inventory FROM impulse_pd WHERE steamid = '"..self:SteamID().."'")
 end
