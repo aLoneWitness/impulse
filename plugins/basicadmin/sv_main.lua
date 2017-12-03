66082544ba6995038d5350617d3898c88bef783b
Jake Green <jaketg10@gmail.com>
2017-12-03 13:04:08 +0000

    Added copyright notice.

--- plugins/basicadmin/sv_main.lua
+++ plugins/basicadmin/sv_main.lua
@@ -1,3 +1,8 @@
+/*
+** Copyright (c) 2017 Jake Green (TheVingard)
+** This file is private and may not be shared, downloaded, used or sold.
+*/
+
 impulse.ba = {} -- Define a table for basic admin
 
 function impulse.ba.Ban(steamid, reason, time, isGlobal)
