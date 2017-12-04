6f62d7c3e595e81cfbfb96bc2ce1d3fc7c765a5d
Jake Green <jaketg10@gmail.com>
2017-12-04 13:20:12 +0000

    Update sv_currency.lua

--- gamemode/core/sv_currency.lua
+++ gamemode/core/sv_currency.lua
@@ -1,3 +1,8 @@
+/*
+** Copyright (c) 2017 Jake Green (TheVingard)
+** This file is private and may not be shared, downloaded, used or sold.
+*/
+
 function meta:SetMoney(value)
     self:setIVar("money",value)
     impulse.DB.query("UPDATE impulse_pd SET money = "..value.." WHERE steamid = '"..self:SteamID().." '")
