0e8e113cf5deee5d3ae630506d0817b133f4a0fd
Jake Green <jaketg10@gmail.com>
2017-12-04 13:20:00 +0000

    Update sv_inventory.lua

--- gamemode/core/sv_inventory.lua
+++ gamemode/core/sv_inventory.lua
@@ -1,3 +1,8 @@
+/*
+** Copyright (c) 2017 Jake Green (TheVingard)
+** This file is private and may not be shared, downloaded, used or sold.
+*/
+
 function meta:inventorySave()
     return impulse.DB.query("UPDATE impulse_pd SET inventory = "..SQLStr(pon.endcode(self.Inventory)).."WHERE steamid = '"..self:SteamID().."'","INVENTORY-SET")
 end
