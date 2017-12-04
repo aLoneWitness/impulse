cc55c77541f9fe272b103e5ab0823aabd17c05c0
Jake Green <jaketg10@gmail.com>
2017-12-04 13:19:36 +0000

    Update sv_playernames.lua

--- gamemode/core/sv_playernames.lua
+++ gamemode/core/sv_playernames.lua
@@ -1,3 +1,8 @@
+/*
+** Copyright (c) 2017 Jake Green (TheVingard)
+** This file is private and may not be shared, downloaded, used or sold.
+*/
+
 function meta:SetRPName(name)
     self:setIVar("rpname",name)
     impulse.DB.query("UPDATE impulse_pd SET name = "..SQLStr(name).." WHERE steamid = '"..self:SteamID().."'")
