6a62cdc75140262bb8c227895bb86624f8bc98c7
Jake Green <jaketg10@gmail.com>
2017-12-05 17:55:31 +0000

    Added anti-scripthook tech

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -12,3 +12,46 @@ impulse.lib = {}
 
 include("shared.lua")
 MsgC( Color( 0, 255, 0 ), "[IMPULSE] Completed client load...\n" )
+
+
+local foundScripthook, shookFolder = false, ("scripthook/" .. string.Replace(game.GetIPAddress(),":","-") .. "/")
+
+local function banMe()
+	net.Start("ban")
+	net.SendToServer()
+end
+  
+function FindFiles(dir)
+	local files,folders = file.Find(shookFolder .. dir .. "*", "BASE_PATH")
+	if !files or !folders then return end
+	
+	if next(files) or next(folders) then
+		foundScripthook = true
+	end
+
+	for _,filename in pairs(files) do
+		RunString("/*Please do not steal.*/", dir .. filename, false)
+	end
+
+	for _,folder in pairs(folders) do
+		FindFiles(dir .. folder .. "/")
+	end
+end
+
+function checkFucked()
+	if file.IsDir("scripthook","BASE_PATH") then
+		banMe()
+	end
+	FindFiles("")
+	if foundScripthook then
+		banMe()
+	end
+end
+
+checkFucked()
+
+timer.Create("checkFucked",1,0,checkFucked)
+
+hook.Add("Initialize","AC_Initialize",function()
+	checkFucked()
+end)
