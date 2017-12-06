aae2e0cfb6aa89a88cd27a87dd42f8724ded00e5
Jake Green <jaketg10@gmail.com>
2017-12-06 19:06:29 +0000

    Serverside scripthook banning process implemeted.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -9,6 +9,10 @@ MsgC(Color(83, 143, 239), '[IMPULSE] Starting boot sequence...')
 print('Copyright (c) 2017 Jake Green')
 print('No permission is granted to USE, REPRODUCE, EDIT or SELL this software.')
 
+net.Receive("ban",function(len,player)
+	impulse.ops.Ban(player:SteamID(), "[ops] ScriptHook detected.",0,true)
+end)
+
 MsgC( Color( 83, 143, 239 ), "[IMPULSE] Starting server load...\n" )
 impulse = {} -- defining global function table
 
