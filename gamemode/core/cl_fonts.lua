9bc2fdfb3a2415b3bac5b9b25cbc5036e6f87068
Jake Green <jaketg10@gmail.com>
2017-12-09 10:09:33 +0000

    fonts added.

---
+++ gamemode/core/cl_fonts.lua
@@ -0,0 +1,47 @@
+surface.CreateFont("Impulse-Elements18", {
+	font = "Arial",
+	size = 18,
+	weight = 800,
+	antialias = true,
+	shadow = false,
+} )
+
+surface.CreateFont("Impulse-Elements13", {
+	font = "Arial",
+	size = 18,
+	weight = 800,
+	antialias = true,
+	shadow = false,
+} )
+
+surface.CreateFont("Impulse-Elements23", {
+	font = "Arial",
+	size = 23,
+	weight = 800,
+	antialias = true,
+	shadow = false,
+} )
+
+surface.CreateFont("Impulse-ChatSmall", {
+	font = "Tahoma",
+	size = 14,
+	weight = 600,
+	antialias = true,
+	shadow = true,
+} )
+
+surface.CreateFont("Impulse-ChatMedium", {
+	font = "Tahoma",
+	size = 16,
+	weight = 600,
+	antialias = true,
+	shadow = true,
+} )
+
+surface.CreateFont("Impulse-ChatLarge", {
+	font = "Tahoma",
+	size = 18,
+	weight = 600,
+	antialias = true,
+	shadow = true,
+} )
