bd83a1534699a8b04ee7c331c48259b7f9aae92c
Jake Green <jaketg10@gmail.com>
2017-12-21 16:08:44 +0000

    Animation update

--- gamemode/config/sh_schemaconfig.lua
+++ gamemode/config/sh_schemaconfig.lua
@@ -7,5 +7,13 @@ impulse.Config.WalkSpeed = 100
 impulse.Config.JogSpeed = 200
 impulse.Config.SprintSpeed = 300
 
+impulse.Config.Languages = impulse.Config.Languages or {}
 
 
+impulse.Config.Languages.Default['English'] = 'https://raw.githubusercontent.com/Apex-Roleplay/bmrp-languagedb/master/english.json'
+impulse.Config.Languages.Addition['French'] = 'https://raw.githubusercontent.com/Apex-Roleplay/bmrp-languagedb/master/french.json'
+impulse.Config.Languages.Addition['German'] = 'https://raw.githubusercontent.com/Apex-Roleplay/bmrp-languagedb/master/german.json'
+impulse.Config.Languages.Addition['Italian'] = 'https://raw.githubusercontent.com/Apex-Roleplay/bmrp-languagedb/master/italian.json'
+impulse.Config.Languages.Addition['Swedish'] = 'https://raw.githubusercontent.com/Apex-Roleplay/bmrp-languagedb/master/swedish.json'
+impulse.Config.Languages.Addition['Dutch'] = 'https://raw.githubusercontent.com/Apex-Roleplay/bmrp-languagedb/master/dutch.json'
+impulse.Config.Languages.Addition['Hungarian'] = 'https://raw.githubusercontent.com/Apex-Roleplay/bmrp-languagedb/master/hungarian.json'
--- gamemode/core/sh_animations.lua
+++ gamemode/core/sh_animations.lua
@@ -7,10 +7,10 @@ hook.Add("CalcMainActivity", "IMPULSE-ANIMATIONBASE", function( Player, Velocity
 	if Player:IsOnGround() and not Player:InVehicle() then
 		if speed <= Player:GetWalkSpeed() + 10 then
 			return ACT_HL2MP_WALK, -1
-		elseif speed > Player:GetRunSpeed() - 10 then
-			return ACT_MP_RUN, -1
 		elseif speed > impulse.Config.SprintSpeed - 10 then
 			return ACT_HL2MP_RUN_FAST, -1
+		elseif speed > Player:GetRunSpeed() - 10 then
+			return ACT_MP_RUN, -1
 		end
 	end
 
