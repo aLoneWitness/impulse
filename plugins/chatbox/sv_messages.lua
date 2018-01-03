535c6f4f5a52182ad192e11bfdb304f6a66fb57b
Jake Green <jaketg10@gmail.com>
2018-01-03 13:38:41 +0000

    Client>Server chat networking finished.

--- plugins/chatbox/sv_messages.lua
+++ plugins/chatbox/sv_messages.lua
@@ -1,6 +1,8 @@
 netstream.Hook("msg", function(client, text)
 		if ((client.NextChat or 0) < CurTime() and text:find("%S")) then
-			hook.Run("PlayerSay", client, text)
+			if hook.Run("PlayerSay", client, text) then
+				client:Say(text, false)
+			end
 			client.NextChat = CurTime() + math.max(#text / 250, 0.4)
 		end
 end)
