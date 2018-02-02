ebf1fca41cb3e771bd03d3fc8cba0dd006dd71ef
Jake Green <jaketg10@gmail.com>
2018-02-02 17:11:13 +0000

    Settings system has been programmed.

--- plugins/settings/cl_settings.lua
+++ plugins/settings/cl_settings.lua
@@ -1,7 +1,33 @@
-function impulse.GetSetting(name,fallback)
+impulse.Settings = impulse.Settings or {}
 
+function impulse.DefineSetting(settingdata)
+	table.insert(impulse.Settings,settingdata)
 end
 
-function impulse.SetSetting()
+function impulse.GetSetting(name)
+	for v,k in pairs(impulse.Settings) do
+		if k.name == name then
+			return k.value
+		end
+	end
+end
 
+function impulse.LoadSettings()
+	for v,k in pairs(impulse.Settings) do
+		if k.type == "tickbox" or k.type == "slider" then
+			k.value = cookie.GetNumber("impulse-setting-"..k.name, k.default) -- Cache the data into a variable instead of sql so its fast
+		elseif k.type == "dropdown" or k.type == "textbox" then
+			k.value = cookie.GetString("impulse-setting-"..k.name, k.default)
+		end
+	end
+end
+
+
+function impulse.SetSetting(name)
+	for v,k in pairs(impulse.Settings) do
+		if k.name == name then
+			return cookie.Set("impulse-setting-"..k.name, k.default)
+		end
+	end
+	return print("[impulse] Error, could not SetSetting. You've probably got the name wrong!")
 end
