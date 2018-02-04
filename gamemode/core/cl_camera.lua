2a2c0fdd81674c4ffce348ae564f1ed2bdb53ea5
Jake Green <jaketg10@gmail.com>
2018-02-04 17:07:39 +0000

    First person death

---
+++ gamemode/core/cl_camera.lua
@@ -0,0 +1,14 @@
+function IMPULSE:CalcView(player, origin, angles, fov)
+	local ragdoll = pl:GetRagdollEntity();
+
+	if( !ragdoll || ragdoll == NULL || !ragdoll:IsValid() ) then return; end
+
+	local eyes = ragdoll:GetAttachment( ragdoll:LookupAttachment( "eyes" ) );
+	local view = {
+		origin = eyes.Pos,
+		angles = eyes.Ang,
+		fov = 70,
+	};
+
+	return view
+end
