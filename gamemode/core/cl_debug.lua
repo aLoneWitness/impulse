a957db175e8db7e0937c75f78071f2cadd6f7034
Jake Green <jaketg10@gmail.com>
2019-04-16 20:54:33 +0100

    Added wtl debug command to allow easy addition of chairs

--- gamemode/core/cl_debug.lua
+++ gamemode/core/cl_debug.lua
@@ -19,3 +19,32 @@ end)
 concommand.Add("impulse_debug_hudtoggle", function(ply)
 	impulse_DevHud = !impulse_DevHud
 end)
+
+concommand.Add("impulse_debug_wtl", function(ply)
+	local traceEnt = LocalPlayer():GetEyeTrace().Entity
+
+	if not traceEnt or not IsValid(traceEnt) then
+		return chat.AddText("You must be looking at an entity.")
+	end
+
+	if impulse_DebugTargPos then
+		local pos = traceEnt:WorldToLocal(impulse_DebugTargPos)
+		local ang = traceEnt:WorldToLocalAngles(impulse_DebugTargAng)
+
+		chat.AddText("Base entity selected. World-To-Local output below and in console:")
+
+		local output = "Vector("..pos.x..", "..pos.y..", "..pos.z..")"
+		chat.AddText(output)
+
+		local output = "Angle("..ang.p..", "..ang.y..", "..ang.r..")"
+		chat.AddText(output)
+		
+		impulse_DebugTargAng = nil
+		impulse_DebugTargPos = nil
+		return
+	end
+
+	impulse_DebugTargPos = traceEnt:GetPos()
+	impulse_DebugTargAng = traceEnt:GetAngles()
+	chat.AddText("Target entity selected as "..tostring(traceEnt)..". Please run the command looking at the child entity for output.")
+end)
