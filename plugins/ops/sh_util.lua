642dddf83d03d60ff6ce9aab3de9cba984ff6312
Jake Green <jaketg10@gmail.com>
2019-05-01 21:16:27 +0100

    Added /kick

--- plugins/ops/sh_util.lua
+++ plugins/ops/sh_util.lua
@@ -20,3 +20,34 @@ local setHealthCommand = {
 }
 
 impulse.RegisterChatCommand("/sethealth", setHealthCommand)
+
+local kickCommand = {
+    description = "Kicks the specified player from the server.",
+    requiresArg = true,
+    adminOnly = true,
+    onRun = function(ply, arg, rawText)
+        local name = arg[1]
+        local plyTarget = impulse.FindPlayer(name)
+
+        local reason = ""
+
+        for v,k in pairs(arg) do
+            if v != 1 then
+                reason = reason.." "..k
+            end
+        end
+
+        reason = string.Trim(reason)
+
+        if reason == "" then reason = nil end
+
+        if plyTarget and ply != plyTarget then
+            ply:Notify("You have kicked "..plyTarget:Name().." from the server.")
+            plyTarget:Kick(reason or "Kicked by a game moderator.")
+        else
+            return ply:Notify("Could not find player: "..tostring(name))
+        end
+    end
+}
+
+impulse.RegisterChatCommand("/kick", kickCommand)
