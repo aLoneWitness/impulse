0b31db3952f6aab42a8355d3db098c46b73ed4a1
Jake Green <jaketg10@gmail.com>
2020-01-31 12:38:29 +0000

    dammit

---
+++ gamemode/core/sh_rpgroup.lua
@@ -0,0 +1,10 @@
+RPGROUP_PERMISSIONS = {
+	[1] = "See group chat",
+	[2] = "Post to group chat",
+	[3] = "Can add members",
+	[4] = "Can remove members",
+	[5] = "Can promote/demote members",
+	[6] = "Can edit ranks"
+	--[7] = "Access group storage",
+	--[8] = ""
+}
--- gamemode/core/sv_sql.lua
+++ gamemode/core/sv_sql.lua
@@ -12,6 +12,7 @@ function IMPULSE:DatabaseConnected()
         sqlQuery:Create("steamid", "varchar(25) NOT NULL") -- steamid
         sqlQuery:Create("group", "varchar(70) NOT NULL") -- usergroup
         sqlQuery:Create("rpgroup", "int(11) unsigned NOT NULL") -- rpgroup
+        sqlQuery:Create("rpgrouprank", "varchar(255) NOT NULL") -- rpgroup rank string
         sqlQuery:Create("xp", "int(11) unsigned DEFAULT NULL") -- xp
         sqlQuery:Create("money", "int(11) unsigned DEFAULT NULL") -- money
         sqlQuery:Create("bankmoney", "int(11) unsigned DEFAULT NULL") -- banked money
@@ -56,6 +57,18 @@ function IMPULSE:DatabaseConnected()
         sqlQuery:Create("steamid", "varchar(25) NOT NULL")
         sqlQuery:Create("item", "varchar(75) NOT NULL")
         sqlQuery:Create("date", "int(11) unsigned NOT NULL")
+        sqlQuery:PrimaryKey("id")
+    sqlQuery:Execute()
+
+    local sqlQuery = mysql:Create("impulse_rpgroups")
+        sqlQuery:Create("id", "int unsigned NOT NULL AUTO_INCREMENT")
+        sqlQuery:Create("ownerid", "int(11) unsigned DEFAULT NULL") -- owner db id
+        sqlQuery:Create("name", "varchar(255) NOT NULL")
+        sqlQuery:Create("type", "int(11) unsigned NOT NULL")
+        sqlQuery:Create("maxsize", "int(11) unsigned NOT NULL")
+        sqlQuery:Create("maxstorage", "int(11) unsigned NOT NULL")
+        sqlQuery:Create("ranks", "longtext")
+        sqlQuery:Create("data", "longtext")
         sqlQuery:PrimaryKey("id")
     sqlQuery:Execute()
 end
