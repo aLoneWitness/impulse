27ce468c2a96a299971ce9df26bd9bbab2b4c989
Jake <jaketg10@gmail.com>
2020-06-23 20:55:27 +0100

    fix

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -16,5 +16,26 @@ MsgC( Color( 0, 255, 0 ), "[impulse] Completed client load...\n" )
 timer.Remove("HintSystem_OpeningMenu")
 timer.Remove("HintSystem_Annoy1")
 timer.Remove("HintSystem_Annoy2")
-hook.Remove("PreDrawHalos", "PropertiesHover")
+
+hook.Add( "PreDrawHalos", "PropertiesHover", function() -- overwrite exploitable context menu shit
+
+	if ( !IsValid( vgui.GetHoveredPanel() ) || !vgui.GetHoveredPanel():IsWorldClicker() ) then return end
+
+	local ent = properties.GetHovered( EyePos(), LocalPlayer():GetAimVector() )
+	if ( !IsValid( ent ) ) then return end
+
+	if ent:GetNoDraw() then
+		return
+	end
+
+	local c = Color( 255, 255, 255, 255 )
+	c.r = 200 + math.sin( RealTime() * 50 ) * 55
+	c.g = 200 + math.sin( RealTime() * 20 ) * 55
+	c.b = 200 + math.cos( RealTime() * 60 ) * 55
+
+	local t = { ent }
+	if ( ent.GetActiveWeapon && IsValid( ent:GetActiveWeapon() ) ) then table.insert( t, ent:GetActiveWeapon() ) end
+	halo.Add( t, c, 2, 2, 2, true, false )
+end )
+
 RunConsoleCommand("cl_showhints",  "0") -- disable annoying gmod hints by default
