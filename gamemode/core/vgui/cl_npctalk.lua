fb4d04d57820341e2505ef68a9971ef78ba83172
Jake <commits.vin@gmail.com>
2021-03-31 23:32:55 +0100

    hotfix

--- gamemode/core/vgui/cl_npctalk.lua
+++
@@ -1,8 +0,0 @@
-local idleVO = {
-	"question23.wav", -- i can't get this tune outta my head
-	"question25.wav", -- i just knew it was gonna be one of those days
-	"question09.wav",
-	"question06.wav",
-	"question05.wav"
-}
-
